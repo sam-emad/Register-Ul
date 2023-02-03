c0302eda9949f8fc63976602bcfd8070680d870c
samvictor-code <113173943+samvictor-code@users.noreply.github.com>
2023-02-03 14:35:58 +0000

    update

--- lib/static/static.dart
+++ lib/static/static.dart
@@ -8,22 +8,3 @@ const Color kPrimerycolor = Color(0xff25D7FE);
 
 
 
-// class NewItem extends StatelessWidget {
-//   const NewItem({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: List.generate(3, (index) {
-//         scrollDirection:
-//         Axis.horizontal;
-//         return Container(
-//           height: 25,
-//           width: 8,
-//           decoration: BoxDecoration(
-//               borderRadius: BorderRadius.circular(8), color: kPrimerycolor),
-//         );
-//       }),
-//     );
-//   }
-// }
